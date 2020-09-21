eaba51e3d76131a4fe0d009ca2808295aa5e0154
Roy Sivan <roy@roysivan.com>
2020-09-20 20:47:12 -0400

    renaming Header test

--- src/components/header/SiteHeader.test.tsx
+++ src/components/header/SiteHeader.test.tsx
@@ -9,33 +9,32 @@ import { HeaderProps } from "./SiteHeader";
 import SiteHeader from "./SiteHeader";
 
 describe("SiteHeader", () => {
+  let changeHandler: jest.Mock<any, any>;
+  let header: RenderResult;
+  let inputElement: HTMLElement;
+  let newSubreddit: string;
+
+  beforeEach(async () => {
+    changeHandler = jest.fn();
+    header = render(<SiteHeader changeSubCallback={changeHandler} />);
+    inputElement = header.getByLabelText("Change Subreddit:");
+    newSubreddit = "hiroy";
+  });
+
+  it("should set input field properly", () => {
+    userEvent.type(inputElement, newSubreddit);
+    expect(inputElement).toHaveValue(newSubreddit);
+  });
+
   describe("changeSubCallback", () => {
-
-    let changeHandler : jest.Mock<any, any>;
-    let header : RenderResult;
-    let inputElement : HTMLElement;
-    let newSubreddit : string;
-
-    beforeEach(async() => {
-      changeHandler = jest.fn();
-      header = render(<SiteHeader changeSubCallback={changeHandler} />);
-      inputElement = header.getByLabelText("Change Subreddit:");
-      newSubreddit = "hiroy";
-    });
-
-    it("should set input field properly", () => {
-      userEvent.type(inputElement, newSubreddit);
-      expect(inputElement).toHaveValue(newSubreddit);
-    });
-
-    it("should call set subreddit callback when string", () => {
+    it("should be called with a string", () => {
       userEvent.type(inputElement, newSubreddit);
       userEvent.click(header.getByTestId("change-subreddit-submit"));
       expect(changeHandler).toHaveBeenCalledTimes(1);
       expect(changeHandler).toBeCalledWith(newSubreddit);
     });
 
-    it('should not call callback if no value', () => {
+    it("should not be called when input field is empty", () => {
       expect(inputElement).toBeEmpty();
       userEvent.click(header.getByTestId("change-subreddit-submit"));
       expect(changeHandler).toHaveBeenCalledTimes(0);
